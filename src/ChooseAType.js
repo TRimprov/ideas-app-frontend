5a4d0721df289365ff0c02829a6c37d8f7aa58c7
Nichola Riley <nicholajaneriley@gmail.com>
2020-02-17 12:27:27 +0000

    css for favourite checkbox green

--- src/ChooseAType.js
+++ src/ChooseAType.js
@@ -18,19 +18,24 @@ class ChooseAType extends React.Component {
                 <fieldset>
                     <div>
                         <div>
-                            <select defaultValue="" id="type" name="type">
+                            <select defaultValue="" id="type" name="type" className="bottom-space">
                                 <option value="" disabled>Choose your type</option>
                                 {this.props.types.map(type =>
-                                     <option key={type.typeId} value={type.typeId}>{type.type}</option>)}
+                                    <option key={type.typeId} value={type.typeId}>{type.type}</option>)}
 
                             </select>
+
+
+                            <div className="custom-sq">
+                                <input type="checkbox" id="box3" />
+                                <label for="box3"><i className="fa fa-heart green-text" aria-hidden="true"></i></label>
+                            </div>
 
 
                             <button
                                 type="submit"
                                 className="btn btn-outline-success btn-sm left-space"
-                            >
-                                Choose
+                            >Choose
                         </button>
                         </div>
                     </div>
