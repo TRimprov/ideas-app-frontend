76a0981ef35fc8ef04ee37c6e25a9d1ab8c248c9
Nichola Riley <nicholajaneriley@gmail.com>
2020-02-21 10:03:07 +0000

    Improved header spacing at margin-top

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Header extends React.Component {
     render () {
         return (
+            <div className="top-space">
             <h4>{this.props.header}</h4>
+            </div>
         )
     }
 }
