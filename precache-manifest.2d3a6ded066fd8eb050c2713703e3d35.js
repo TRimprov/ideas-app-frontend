a3095020e454438195623595a1240f7edc921620
Nichola Riley <nicholajaneriley@gmail.com>
2020-02-22 09:45:20 +0000

    Updates

---
+++ precache-manifest.2d3a6ded066fd8eb050c2713703e3d35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efb6c3932a7362458115d3ac899b033",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47190e97907ff58876af",
+    "url": "/static/css/main.93453cba.chunk.css"
+  },
+  {
+    "revision": "ae4678c224c6144d57ab",
+    "url": "/static/js/2.eedd4aa5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.eedd4aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47190e97907ff58876af",
+    "url": "/static/js/main.cf55ad89.chunk.js"
+  },
+  {
+    "revision": "a7a38b463f3af23d2e35",
+    "url": "/static/js/runtime-main.e826f85c.js"
+  }
+]);
